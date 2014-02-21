ff920cc9c6f9e4e71d17cdf738df2f0bd6730fd0
Chris Cornutt <enygma@phpdeveloper.org>
2014-02-21 15:33:14 -0600

    updating SMS templates with more logic

--- templates/sms-sent.php
+++ templates/sms-sent.php
@@ -1,1 +1,10 @@
-You code was sent to <?php echo $phone; ?>!
+<?php if ($error): ?>
+	<div class="alert alert-warning"><?php echo $error; ?></div>
+<?php else: ?>
+<h3>Your code was sent to <?php echo $phone; ?>!</h3>
+<br/>
+<p>
+	When you recieve the code, go back to the <a href="/sms">SMS main page</a>,
+	enter your email address and code and click "Verify" to validate.
+</p>
+<?php endif; ?>
--- templates/sms.php
+++ templates/sms.php
@@ -1,3 +1,25 @@
+<script type="text/javascript">
+$(function() {
+    $('#validate-sms').click(function(e){
+        e.preventDefault();
+        $.get('/verify-sms.php?code='+$('#sms-code').val()+'&email='+$('#sms-email').val(),
+        function(data) {
+            console.log(data);
+
+            if (data == 'pass') {
+                var style = 'success';
+                var message = 'Code validated!';
+            } else if (data == 'fail') {
+                var style = 'danger';
+                var message = 'Invalid code!';
+            }
+            $('#validate-output').html(
+                '<div class="alert alert-'+style+'" style="width:150px">'+message+'</div>'
+            );            
+        });
+    });
+});
+</script>
 <h2>SMS-based</h2>
 <p>
     There's a number of SMS-based two-factor options available out there. They range all the way from hosted services
@@ -13,13 +35,13 @@
 </p>
 <form action="/sms/send" method="POST" role="form" class="form-horizontal">
     <div class="form-group">
-        <label for="phone">Phone Number:</label>
+        <label for="phone">Phone Number:</label><br/>
         <div class="col-sm-4">
             <input type="text" name="phone" size="25" value="" class="form-control" placeholder="Example: +12145551234"/>
         </div>
     </div>
     <div class="form-group">
-        <label for="email">Email Address:</label>
+        <label for="email">Email Address:</label><br/>
         <div class="col-sm-4">
             <input type="text" name="email" size="25" value="" class="form-control" placeholder="foo@bar.com"/>
         </div>
@@ -32,26 +54,27 @@
 </form>
 
 <h3>Verify the code</h3>
+<span id="validate-output"></span>
 <p>
     Once you recieve the message, it will contain a six digit code - enter this below along with your email address
     and you'll get a "valid" or "invalid" response.
 </p>
 <form action="/sms/verify" method="POST" role="form" class="form-horizontal">
     <div class="form-group">
-        <label for="email">Email Address:</label>
+        <label for="email">Email Address:</label><br/>
         <div class="col-sm-4">
-            <input type="text" name="email" size="25" value="" class="form-control" placeholder="foo@bar.com"/>
+            <input type="text" id="sms-email" name="email" size="25" value="" class="form-control" placeholder="foo@bar.com"/>
         </div>
     </div>
     <div class="form-group">
-        <label for="code">Code:</label>
+        <label for="code">Code:</label></br>
         <div class="col-sm-4">
-            <input type="text" name="code" size="25" value="" class="form-control" placeholder="Code from SMS"/>
+            <input type="text" id="sms-code" name="code" size="25" value="" class="form-control" placeholder="Code from SMS"/>
         </div>
     </div>
     <div class="form-group">
         <div class="col-sm-offset-3 col-sm-4">
-            <button type="submit" name="submit" class="btn btn-primary">Validate</button>
+            <button id="validate-sms" class="btn btn-primary">Validate</button>
         </div>
     </div>
 </form>
